d46354bd6343b3e931d12b56d96f60369d800f56
Logan Cox <mail@logan-cox.com>
2024-02-18 18:30:34 +0000

    feat: update seed

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import random
 from window import Window
 from maze import Maze
 
 
 if __name__ == "__main__":
     window = Window(805, 805)
-    maze = Maze(5, 5, 10, 10, 80, 80, window)
+    maze = Maze(5, 5, 10, 10, 80, 80, window, random.randint(0, 1000))
     window.wait_for_close()
